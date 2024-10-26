d587776401802a86b5c4b3b4cbf8a05b92fb86f2
ludvikkristoffersen <ludvikkristoffersen@protonmail.com>
2024-10-26 23:00:26 +0200

    Added task 2 to solutions

---
+++ solutions/counter.js
@@ -0,0 +1,12 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+function createCounter(n) {
+	return () => {
+		return n++;
+	};
+}
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
